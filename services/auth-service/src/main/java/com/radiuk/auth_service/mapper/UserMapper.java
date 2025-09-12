182590fdfeaa6e2fe2e49239344ac1d144ff8f2d
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-09-12 16:31:28 +0300

    refactor: improve mapper methods names

--- services/auth-service/src/main/java/com/radiuk/auth_service/mapper/UserMapper.java
+++ services/auth-service/src/main/java/com/radiuk/auth_service/mapper/UserMapper.java
@@ -1,13 +1,14 @@
 package com.radiuk.auth_service.mapper;
 
-import com.radiuk.auth_service.dto.UserDto;
+import com.radiuk.auth_service.dto.UserResponseDto;
+import com.radiuk.auth_service.dto.UserRegistrationDto;
 import com.radiuk.auth_service.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserDto toDto(User user);
+    UserResponseDto toUserResponseDto(User user);
 
-    User toEntity(UserDto userDto);
+    User fromRegistrationDto(UserRegistrationDto userRegistrationDto);
 }
