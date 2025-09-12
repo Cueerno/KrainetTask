a76bf452c10ed9207c4be071ef116a4d2c96974b
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-09-12 16:33:21 +0300

    feat: add crud methods to UserService - getMe, updateMe and deleteMe(by id)

--- services/auth-service/src/main/java/com/radiuk/auth_service/service/UserService.java
+++ services/auth-service/src/main/java/com/radiuk/auth_service/service/UserService.java
@@ -1,12 +1,18 @@
 package com.radiuk.auth_service.service;
 
-import com.radiuk.auth_service.dto.UserDto;
+import com.radiuk.auth_service.dto.UserResponseDto;
+import com.radiuk.auth_service.dto.UserUpdateDto;
 import com.radiuk.auth_service.exception.UserNotFoundException;
+import com.radiuk.auth_service.exception.UserNotUpdatedException;
 import com.radiuk.auth_service.mapper.UserMapper;
+import com.radiuk.auth_service.model.User;
 import com.radiuk.auth_service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +21,36 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserDto getMe(Jwt jwt) {
-        String email = (String) jwt.getClaims().get("email");
+    @Transactional(readOnly = true)
+    public UserResponseDto getMe(Jwt jwt) {
+        return userRepository.findByEmail(jwt.getSubject())
+                .map(userMapper::toUserResponseDto)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
 
-        return userRepository.findByEmail(email)
-                .map(userMapper::toDto)
-                .orElseThrow(() -> new UserNotFoundException(email));
+    @Transactional(readOnly = true)
+    public void updateMe(UserUpdateDto userUpdateDto, Jwt jwt) {
+        if (userRepository.existsByEmail(userUpdateDto.email())) {
+            throw new UserNotUpdatedException("User with this email already exists");
+        }
+        if (userRepository.existsByUsername(userUpdateDto.username())) {
+            throw new UserNotUpdatedException("User with this username already exists");
+        }
+
+        User user = userRepository.findByEmail(jwt.getSubject())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        Optional.ofNullable(userUpdateDto.username()).ifPresent(user::setUsername);
+        Optional.ofNullable(userUpdateDto.email()).ifPresent(user::setEmail);
+        Optional.ofNullable(userUpdateDto.firstname()).ifPresent(user::setFirstname);
+        Optional.ofNullable(userUpdateDto.lastname()).ifPresent(user::setLastname);
+    }
+
+
+    public void deleteMe(Jwt jwt) {
+        User user = userRepository.findByEmail(jwt.getSubject())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        userRepository.deleteById(user.getId());
     }
 }
