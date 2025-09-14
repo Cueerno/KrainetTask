794f66b55b884168b28d2d0e4ee9f6d62dd823ea
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-09-14 17:33:43 +0300

    feat: add method for return admins

--- services/auth-service/src/main/java/com/radiuk/auth_service/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/radiuk/auth_service/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.radiuk.auth_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    Boolean existsByEmail(String email);
     boolean existsByEmail(String email);
 
+    List<User> findAllByRole(User.Role role);
 }
