c72b7e8a958884351e72b93cd10607fd95f93dc8
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-09-14 17:33:18 +0300

    feat: add publisher and listener

---
+++ services/auth-service/src/main/java/com/radiuk/auth_service/publisher/UserEventPublisher.java
@@ -0,0 +1,53 @@
+package com.radiuk.auth_service.publisher;
+
+import com.radiuk.auth_service.config.RabbitConfig;
+import com.radiuk.auth_service.event.UserNotificationMessage;
+import com.radiuk.auth_service.model.User;
+import com.radiuk.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserEventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final UserRepository userRepository;
+
+    public void publishUserEvent(User user, String action) {
+        List<User> admins = userRepository.findAllByRole(User.Role.ADMIN);
+
+        for (User admin : admins) {
+            UserNotificationMessage message = new UserNotificationMessage(
+                    admin.getEmail(),
+                    String.format("User {%s} %s", user.getUsername(), action),
+                    String.format("User %s with username - {%s}, email - {%s}",
+                            action, user.getUsername(), user.getEmail())
+            );
+
+            String correlationId = UUID.randomUUID().toString();
+
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                public void afterCommit() {
+                    rabbitTemplate.convertAndSend(
+                            RabbitConfig.EXCHANGE_NAME,
+                            RabbitConfig.ROUTING_KEY,
+                            message,
+                            msg -> {
+                                msg.getMessageProperties().setCorrelationId(correlationId);
+                                msg.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
+                                return msg;
+                            }
+                    );
+                }
+            });
+        }
+    }
+}
---
+++ services/notification-service/src/main/java/com/radiuk/notification_service/listener/UserEventsListener.java
@@ -0,0 +1,28 @@
+package com.radiuk.notification_service.listener;
+
+import com.radiuk.notification_service.config.RabbitConfig;
+import com.radiuk.notification_service.event.UserNotificationMessage;
+import com.radiuk.notification_service.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserEventsListener {
+
+    private final EmailService emailService;
+
+    @RabbitListener(queues = RabbitConfig.QUEUE_NAME)
+    public void handleUserNotification(UserNotificationMessage message) {
+        if (message == null || message.adminEmail() == null) {
+            log.warn("Received invalid notification message: {}", message);
+            return;
+        }
+
+        emailService.sendEmail(message.adminEmail(), message.subject(), message.text());
+        log.info("Notification sent to {}", message.adminEmail());
+    }
+}
