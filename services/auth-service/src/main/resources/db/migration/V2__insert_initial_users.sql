7fe042ce209602b6724d71ee41e359c67ba99623
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-09-15 17:40:49 +0300

    update V2__insert_initial_users.sql

--- services/auth-service/src/main/resources/db/migration/V2__insert_initial_users.sql
+++ services/auth-service/src/main/resources/db/migration/V2__insert_initial_users.sql
@@ -3,4 +3,4 @@ values
 ('Cuerno','Cuerno','Vyacheslav','Radiuk','vyacheslavradiuk@gmail.com','ADMIN'),-- password: Cuerno
 ('John12','John12','John','Doe','john12@gmail.com','USER'),-- password: John12
 ('User',  'user','Tom','Jerry','user.tom.jerry@gmail.com','USER'),-- password: user
-('admin', 'admin','AdminFirstName','AdminLastName','radukslava104@gmail.com','ADMIN');-- password: admin
+('admin', 'admin','AdminFirstName','AdminLastName','admin@gmail.com','ADMIN');-- password: admin
