d9669e4df3adfbe4926c11a1efb7a5736ed56524
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-12-25 19:34:54 +0300

    feat(auth): add сron cleanup expired refresh tokens

--- services/auth-service/src/main/java/com/radiuk/auth_service/repository/RefreshTokenRepository.java
+++ services/auth-service/src/main/java/com/radiuk/auth_service/repository/RefreshTokenRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
@@ -24,4 +25,12 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     @Modifying
     @Query("update RefreshToken t set t.revoked = true where t.user = :user")
     void revokeAllByUser(@Param("user") User user);
+
+    @Modifying
+    @Query("""
+        delete from RefreshToken rt
+        where rt.expiresAt < :now
+           OR rt.revoked = true
+    """)
+    int deleteExpiredOrRevoked(@Param("now") Instant now);
 }
